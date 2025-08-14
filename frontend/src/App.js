d8fd018f6954c8300d9579060bcd27f1977b0a0a
vpanajwala <vpanajwala@gmail.com>
2025-08-14 14:27:53 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
+
+  return (
+    <div>
+      <h1>{message || "Loading..."}</h1>
+    </div>
+  );
+}
+
+export default App;
